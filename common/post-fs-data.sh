3d2ead96fc3aa9b51f93d37266667e6dbed90f7a
Rom <skav@gmx.fr>
2018-04-03 19:28:10 +0200

    Update post-fs-data.sh to v3.4

--- common/post-fs-data.sh
+++ common/post-fs-data.sh
@@ -8,50 +8,73 @@ MODDIR=${0%/*}
 
 # Additional VARs
 COMMONFILES=$MODDIR/common/
-OPERMISSIONS=$MODDIR/system/etc/O-permissions/
-PERMDIR=$MODDIR/system/etc/permissions/
-OAPPLIST=ExSDCard.O.*.applist.xml
-INTERNALPATH=/data/media/0
+BUILDPROP=/system/build.prop
+SYSTEMLESSPROP=$MODDIR/system/build.prop
+PHONEID=`grep_prop ro.product.device $BUILDPROP`
+ANDROVERSION=`grep_prop ro.build.version.release $BUILDPROP`
+SYSSEMODPATH=$MODPATH/system/etc/init.d/
+SYSSEMODPATHFILE=$MODPATH/system/etc/init.d/selinux0
+PERMDIR=$MODDIR/system/etc/permissions
+SYSPERMDIRFILE=/system/etc/permissions/platform.xml
+OPERMISSIONS=$MODDIR/system/etc/O-permissions
+EXOREOAPP=$MODDIR/ExSDCard_Oreo_apps
+EXOREOAPPBAK=$MODDIR/ExSDCard_Oreo_apps.bak
 
-# Check if an platform.xml file already exist in PERMDIR or not
-cd $MODDIR/ 
-	rm -f Special_XML_perm_file_result.txt
-cd $PERMDIR/
-if [ -f "platform.xml" ]
-then
-	printf "Special xml permissions file has been already putted in the MODDIR path.\nIt's due to the fact that you have manually replace "no" by "yes"\nin the "ExSDCard.config.xml" file. If you want to cancel this changes\nand just want an simple edited XML permissions file, so just replace\n the one "yes" values in the config file by "no".\n\nHave a good day and thanks to @topjhonwu for Magisk :)" > $MODDIR/Special_XML_perm_file_result.txt
+# Edit perm file settings
+mkdir -p $PERMDIR
+cp -af $SYSPERMDIRFILE $PERMDIR
+sed -i '/<group gid=\"sdcard_rw\" \/>/a\        <group gid="sdcard_all" />\' $PERMDIR/platform.xml
+sed -i '/<permission name=\"android\.permission\.WRITE\_EXTERNAL\_STORAGE\" \/>/a\        <group gid=\"sdcard_r\" />\n\ \       \<group gid="sdcard_rw\" />\n\ \       \<group gid="media_rw\" /> ' $PERMDIR/platform.xml
+chmod 644 $PERMDIR/platform.xml
 
+
+# Check if selinux must to be toggle into permissive mode
+cp $BUILDPROP $SYSTEMLESSPROP
+if grep -wqs '8' $SYSTEMLESSPROP
+	if grep -qs 'j5xnlte\|tissot_sprout\|kenzo\|manning\|ha3g\|D5803\|D2303\|maple_dsds\|a3y17\|Z00A\|dreamlte\|tissot_sprout\|GT-P7510\|hlte\|dreamlteks\|z3c\|sf340n' $SYSTEMLESSPROP; then
+		mkdir -p $SYSSEMODPATH
+		printf "setenforce 0" >> $SYSSEMODPATHFILE
+		adb shell setenforce 0
+	fi
 else
-
-	# Create the perm dir in $MODDIR
-	yes | cp -a "/system/etc/permissions/platform.xml" ./platform.xml
-
-		# Edit the xml file using sed
-		sed -i '/<group gid=\"sdcard_rw\" \/>/a\        <group gid="sdcard_all" />\' ./platform.xml
-		sed -i '/<permission name=\"android\.permission\.READ\_EXTERNAL\_STORAGE\" \/>/a\        <group gid=\"sdcard_r\" />\n' ./platform.xml
-		sed -i '/<permission name=\"android\.permission\.WRITE\_EXTERNAL\_STORAGE\" \/>/a\        <group gid=\"sdcard_r\" />\n\ \       \<group gid="sdcard_rw\" />\n\ \       \<group gid="media_rw\" /> ' ./platform.xml
-
-	# Edit the file permissions
-	chmod 644 ./platform.xml
-
+	rm -f $SYSTEMLESSPROP
 fi
-mount /data 2>/dev/null
-	if mount | grep -q /data; then
-	cd $INTERNALPATH/
 
-		if ls -al | grep -q "ExSDCard.O."
-	then
-		NEW=$(awk '{ print }' ExSDCard.O.*.applist.txt)
 
-		cd $MODDIR/ && mkdir /TMP/
+# Check if FUSE is enabled in build.prop file
+cp -af $BUILDPROP $SYSTEMLESSPROP
+
+if grep -qs 'ro.sys.sdcardfs=true' $SYSTEMLESSPROP; then
+	sed -i 's/^ro.sys.sdcardfs=true/ro.sys.sdcardfs=false/' $SYSTEMLESSPROP
+fi
+if grep -qs 'persist.esdfs_sdcard=true' $SYSTEMLESSPROP; then
+	sed -i 's/^persist.esdfs_sdcard=false' $SYSTEMLESSPROP
+fi
+if grep -qs 'persist.sys.sdcardfs' $SYSTEMLESSPROP; then
+	sed -i 's/^persist.sys.sdcardfs=true/persist.sys.sdcardfs=false/' $SYSTEMLESSPROP
+fi
+
+# Force ESDFS and SDCardFS to be disabled
+resetprop persist.esdfs_sdcard false || setprop persist.esdfs_sdcard false
+resetprop persist.sys.sdcardfs force_off || setprop persist.sys.sdcardfs force_off
+
+# Force FUSE to be enabled
+resetprop persist.fuse_sdcard true || setprop persist.fuse_sdcard true
+
+if [ -a "$EXOREOAPP" ]; then
+	cp $EXOREOAPP $EXOREOAPPBAK
+	NEW=$(awk '{ print }' $EXOREOAPPBAK)
+	mkdir $OPERMISSIONS
+	printf '<?xml version="1.0" encoding="utf-8"?>\n<permissions>\n  <privapp-permissions package="com.package.name">\n	<permission name="android.permission.DUMP" />\n	<permission name="android.permission.READ_LOGS" />\n	<permission name="android.permission.DEVICE_POWER" />\n  </privapp-permissions>\n</permissions>\n' >> $MODDIR/privapp-permissions-com.package.name.xml
+		
 		printf '%s\n' "$NEW" | while IFS= read -r line
-					do sed "s/com.package.name/$line/" < $MODDIR/privapp-permissions-com.package.name.xml >  $MODDIR/TMP/privapp-permissions-$line.xml
+		do sed "s/com.package.name/$line/" < $MODDIR/privapp-permissions-com.package.name.xml >  $OPERMISSIONS/privapp-permissions-$line.xml
 
-		no | cp $MODDIR/TMP/*.xml $PERMDIR/
+	cp -n $OPERMISSIONS/*.xml $PERMDIR/
 
-		rm -rf ./TMP/
+	rm -rf $OPERMISSIONS/*
+	rm -f $EXOREOAPPBAK
 
-		chmod -R 644 $PERMDIR/*
-		fi
-	else
+	chmod -R 644 $PERMDIR/*
 fi
+
